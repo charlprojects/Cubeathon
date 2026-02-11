1110073bd286f3233d46b28cbe2edbe490ee2bd2
Charl <charllevin2009@gmail.com>
2026-02-11 11:49:06 -0600

    Finished with tutorial video #2

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // This is a reference to the Rigidbody component called "rb"
+
+
+    // We marked this as "Fixed"Update because we
+    // are using it to mess with physics
+    void FixedUpdate()
+    {
+        rb.AddForce (0,0,2000 * Time.deltaTime);
+    }
+}
